ab3de15bb04a787c01b7ab814bea7b48aaa0fbde
lirico <dominguezmatiasadrian@gmail.com>
2025-05-06 20:49:55 -0300

    js clase 1 + tarea coercion

---
+++ js_clase_1/index.js
@@ -0,0 +1,144 @@
+/* 
+    JAVASCRIPT
+
+    Nacio: 1995
+    Creador: Brendan Eich
+    Leyenda: Le pidieron crearlo en 13 dias.
+    Dato de color: Se llama JAVAscript por que el lenguaje mas popular
+    de los 90 era JAVA. Conlusion: Se colgaron de la popularidad.
+
+    Originalmente: Pensado para sitios webs -> Darle interactivida a los sitios webs
+
+    Que se puede hacer con JS:
+    - Desarrollo de videojuegos
+    - Desarrollo de sitios webs
+    - Desarrollo de apps webs
+    - Desarrollo de apps mobile
+    - Programar hardware
+    - Modelaje 3D
+    - Machine Learning
+    - Programar BOTS IA
+    - Desarrollar APP de escritorio.
+    - Robotica
+
+    JS es un lenguaje de TIPADO DEBIL.
+*/
+
+
+// TIPOS DE DATOS -> Primitivos 
+// Representan existencia de valor
+// number -> 32 / -32 / 32.12 / -32.12
+// string -> "hola" / "32" / "a7sd6a7sd6as"
+// boolean -> true / false
+
+// Representan inexistencia de valor
+// undefined
+// null
+// NaN -> Not a Number
+
+// TIPOS DE DATOS -> Compuestos
+// array -> [32,1,5,"hoa",true,2]
+// object -> {nombre: "Pepe", edad: 22}
+// function -> algoritmo ejecutable
+// class -> plantilla que nos permite generar objetos
+
+
+// 1 // number
+
+// "hola" // string
+
+// true // boolean
+
+
+// var numero = 2;
+
+// var numero = "jorge"
+
+
+// console.log(2 + numero + numero)
+
+
+// Operadores aritmeticos
+// Suma +
+// Resta -
+// Multiplicacion *
+// Potencia **
+// Division /
+// Modulo %
+// Incrementadores ++
+// Decrementadores --
+
+// NOTA: El resultado de una operacion artimetica siempre sera un number.
+
+// Operadores de comparacion
+// Mayor que >
+// Menor que <
+// Mayor o igual que >=
+// Menor o igual que <=
+// Igualdad no estricta (no coercitiva) ==
+// Igualdad estricta (coercitiva) ===
+// Desigualdad no estricta (no coercitiva) !=
+// Desigualdad estricta (coercitiva) !==
+
+
+// NOTA: El restulado de una operacion de comparacion siempre sera un booleano.
+
+// console.log(3 == "3") // Checkea valor, pero ignora tipo
+// console.log("----------")
+// console.log(3 === "3") // Checkea valor y tipo
+
+
+// Operadores Logicos
+// AND -> &&
+// OR -> ||
+// NOT -> !
+
+// NOTA: El restulado de una operacion de comparacion siempre sera un booleano.
+
+
+
+// COERCION DE TIPOS DE DATO
+
+// Sumar un numero con otro
+// console.log(3 + 3)
+
+// Sumar un numero con una cadena (string)
+// + -> CONCATENACION
+// console.log(3 + "3") // "3" + "3" -> "33"
+
+// Sumar un numero con un booleano
+// console.log(1 + true) // 1 + 1 (valor binario de true) -> 2
+
+// Multiplicacion entre numero y cadena
+// console.log(3 * "3") // 3 * 3 -> 9
+// console.log(3 * "hola") // 3 * NaN -> NaN
+
+///////////////////////////////////////////////////////////////
+
+// TAREA
+
+// 6 / "3" =
+// "2" * "3" =
+// 4 + 5 + "px" =
+// "$" + 4 + 5 =
+// "4" - 2 =
+// "4px" - 2 =
+// 7 / 0 =
+// 3>2>1 =
+// 12 + ""  =  
+// "15" * 2  = 
+// "15" - "11" =
+// undefined + 6 = 
+// "Hello" + null = 
+// null + 25 =    
+// true + true =
+// false + 10 =
+// 5 && 2 =
+// 2 && 5 =
+// 5 || 0 =
+// 0 || 5 =
+// true && false =
+// false || !false =
+// !2 =
+// "texto" || 0 =
+// 2 || "prueba“ =
